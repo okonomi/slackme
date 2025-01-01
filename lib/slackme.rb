9b71ab21269aeebea737303b4b086cb8504ab09c
okonomi <okonomi@oknm.jp>
2025-01-01 17:24:50 +0900

    feat: implement Slackme CLI

--- lib/slackme.rb
+++ lib/slackme.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "slackme/version"
+require_relative "slackme/cli"
 
 module Slackme
   class Error < StandardError; end
---
+++ lib/slackme/cli.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Slackme
+  class CLI
+    def self.run(argv)
+      puts "Hello, Slackme!"
+    end
+  end
+end
