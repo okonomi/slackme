9964e1adb9c5e54183efbed9e97b747e316fadcb
okonomi <okonomi@oknm.jp>
2025-01-03 22:56:32 +0900

    refactor: simplify HTTP request handling with a dedicated request method

--- lib/slackme/http.rb
+++ lib/slackme/http.rb
@@ -6,21 +6,23 @@ require "uri"
 module Slackme
   class HTTP
     def self.get(url, headers: nil)
-      uri = URI(url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = uri.scheme == "https"
-
-      http.get(uri.path, headers)
+      request(url) do |http, uri|
+        http.get(uri.path, headers)
+      end
     end
 
     def self.post(url, headers: nil, body: nil)
+      request(url) do |http, uri|
+        body = URI.encode_www_form(body) if body.is_a?(Hash)
+        http.post(uri.path, body, headers)
+      end
+    end
+
+    private_class_method def self.request(url)
       uri = URI(url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = uri.scheme == "https"
-
-      body = URI.encode_www_form(body) if body.is_a?(Hash)
-
-      http.post(uri.path, body, headers)
+      Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == "https") do |http|
+        yield(http, uri) if block_given?
+      end
     end
   end
 end
