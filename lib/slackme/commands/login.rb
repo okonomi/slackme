e4741fa07b95a56c0328e87e9e1652104f8be982
okonomi <okonomi@oknm.jp>
2025-01-03 19:34:40 +0900

    refactor: enhance login flow with OAuth2 code exchange and token saving

--- lib/slackme/commands/login.rb
+++ lib/slackme/commands/login.rb
@@ -11,13 +11,35 @@ module Slackme
     class Login
       def call
         puts "Logging in..."
+        client_id, client_secret = gets_oauth2_credentials
 
+        puts "Open the authorize URL in your browser..."
+        Launchy.open(build_authorize_url(client_id))
+
+        puts "Please enter the code from the URL:"
+        print "code: "
+        code = $stdin.gets.chomp
+
+        puts "Exchange the code for an access token..."
+        token_info = exchange_code_for_access_token(code, client_id, client_secret)
+
+        puts "Save token_info to a file..."
+        save_token_info(token_info)
+      end
+
+      private
+
+      def gets_oauth2_credentials
         print "Enter your client ID: "
         client_id = $stdin.gets.chomp
 
         print "Enter your client secret: "
         client_secret = $stdin.gets.chomp
 
+        [client_id, client_secret]
+      end
+
+      def build_authorize_url(client_id)
         authorize_url = URI("https://slack.com/oauth/v2/authorize")
         authorize_url.query = URI.encode_www_form({
                                                     user_scope: "users.profile:read",
@@ -25,22 +47,20 @@ module Slackme
                                                     client_id: client_id
                                                   })
 
-        Launchy.open(authorize_url.to_s)
+        authorize_url
+      end
 
-        puts "Please enter the code from the URL:"
-        print "code: "
-        code = $stdin.gets.chomp
-
-        puts "Exchange the code for an access token..."
+      def exchange_code_for_access_token(code, client_id, client_secret)
         token_url = URI("https://slack.com/api/oauth.v2.access")
         response = Net::HTTP.post_form(token_url, {
                                          code: code,
                                          client_id: client_id,
                                          client_secret: client_secret
                                        })
-        token_info = JSON.parse(response.body)
+        JSON.parse(response.body)
+      end
 
-        puts "Save token_info to a file..."
+      def save_token_info(token_info)
         xdg_config = XDG::Config.new
         config_dir = xdg_config.home.join("slackme")
         config_dir.mkpath
